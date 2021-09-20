dae110df69abd1e89561fc1468fb5ceba5603cd1
rgegajo <rosellegegajo@gmail.com>
2021-09-20 20:35:19 +0800

    #2 - Rename the brand name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import Nav from 'react-bootstrap/Nav'
 export default function AppNavbar() {
     return (
         <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Sample Portfolio</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
