cd70685a5db1b5ad8f9e89233ba65f536072eb89
rgegajo <rosellegegajo@gmail.com>
2021-09-21 21:01:52 +0800

    Added style to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,17 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
+
+
 export default function AppNavbar() {
     return (
         <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+            <h1 style={{color: "pink"}}>React-Bootstrap</h1>
+        </Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="#home" style={{color: "pink"}}>Home</Nav.Link>
+                <Nav.Link href="#link" style={{color: "pink"}} >Link</Nav.Link>
             </Nav>
         </Navbar>
     )
