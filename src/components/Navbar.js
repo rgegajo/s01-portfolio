183e1ec977e6eb51a6ea63519b74781a9bcf07f1
rgegajo <rosellegegajo@gmail.com>
2021-09-22 21:42:50 +0800

    #7 Setup CI/CD using Production

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,10 @@ import Nav from 'react-bootstrap/Nav'
 export default function AppNavbar() {
     return (
         <Navbar bg="light" expand="lg">
-<<<<<<< HEAD
         <Navbar.Brand href="#home">CHENG</Navbar.Brand>
-=======
-        <Navbar.Brand href="#home">Cheng</Navbar.Brand>
->>>>>>> staging
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
-                <Nav.Link href="#link">About</Nav.Link>
             </Nav>
         </Navbar>
     )
