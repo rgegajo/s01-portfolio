41b049b87a5bce3b436aa3beb869903dba4efe97
rgegajo <rosellegegajo@gmail.com>
2021-09-23 20:41:25 +0800

    #7 Add new link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,19 +6,12 @@ import Nav from 'react-bootstrap/Nav'
 export default function AppNavbar() {
     return (
         <Navbar bg="light" expand="lg">
-<<<<<<< HEAD
-        <Navbar.Brand href="#home">CHENG DYosa</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-=======
         <Navbar.Brand href="#home">CHENG DYOSA</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
                 <Nav.Link href="#link">About</Nav.Link>
                 <Nav.Link href="#link">Contact Us</Nav.Link>
->>>>>>> staging
             </Nav>
         </Navbar>
     )
