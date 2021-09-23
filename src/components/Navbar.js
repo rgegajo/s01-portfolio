30846e2fd94336db993e9859c0a367502b2a29fc
rgegajo <rosellegegajo@gmail.com>
2021-09-23 20:13:02 +0800

    #7 add a navlink for Contact Us

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,12 @@ import Nav from 'react-bootstrap/Nav'
 export default function AppNavbar() {
     return (
         <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Cheng</Navbar.Brand>
+        <Navbar.Brand href="#home">CHENG DYOSA</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
                 <Nav.Link href="#link">About</Nav.Link>
+                <Nav.Link href="#link">Contact Us</Nav.Link>
             </Nav>
         </Navbar>
     )
